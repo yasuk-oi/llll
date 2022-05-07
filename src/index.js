6942c1a566d590433f834bf57a738bf8543fed23
yasukoi <67302498+yasuk-oi@users.noreply.github.com>
2022-05-07 16:28:55 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,47 @@
+/**
+ * const,let等の変数宣言
+ */
+
+// var val1 = "変数宣言";
+// console.log(val1);
+
+// //var変数は上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// //var変数は
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// //letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// //letは再宣言不可能
+// let val2 = "let変数を再宣言";
+
+//const val3 = "const変数";
+//console.log(val3);
+
+//const変数は上書き賦課
+//val3 = "const変数を上書き";
+
+//const val3 = "const変数を再宣言";
+
+//constで定義したオブジェクトはプロパティの変更が可能
+// const val4 = {
+//   name: "じゃけえ",
+//   age: 28
+// };
+// val4.name = "jak";
+// val4.address = "Hiroshima";
+// console.log(val4);
+
+//constで定義した配列はプロパティの変更が可能
+const val5 = ["dog", "cat"];
+val5[0] = "bird";
+val5.push("monley");
+console.log(val5);
